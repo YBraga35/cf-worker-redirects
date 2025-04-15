081d651c51663e11dc5c0e8555adb37ac404c047
ybraga35 <silvayanbraga@gmail.com>
2025-04-15 12:21:54 -0300

    Fix redirect URL for 'esperanca-no-deserto' case by removing unnecessary query parameters

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ export default {
         return Response.redirect('https://wa.me/5551991866752?text=Ol%C3%A1!%20Vim%20para%20a%20campanha', 301)
 
       case '/esperanca-no-deserto':
-        return Response.redirect('https://yan-braga.notion.site/Divuga-o-Esperan-a-no-Deserto-1cbf36a87c3b803cb135e9b1b314e7c4?pvs=4', 301)
+        return Response.redirect('https://yan-braga.notion.site/Divuga-o-Esperan-a-no-Deserto-1cbf36a87c3b803cb135e9b1b314e7c4', 301)
       case '/esperanca-no-deserto/whatsapp':
           return Response.redirect('https://wa.me/5551991866752?text=Ol%C3%A1!%20Vim%20para%20a%20vaga%20da%20campanha%20de%20quinta-feira', 301)
 
